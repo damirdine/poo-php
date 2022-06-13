060e7dfc7f7ab7c61d26374aecb4bb4525efaec3
damirdine <damirdine@outlook.fr>
2022-06-13 17:00:37 +0200

    employe class

---
+++ employe.php
@@ -0,0 +1,42 @@
+<?php
+// $date = '10/03/2020';
+// $date1 =new DateTime($date);
+// $date2 = new DateTime();
+// $diff = $date1->diff($date2);
+// var_dump($diff->format('%r%y years'));
+
+class Employe
+{
+    private  $nom = "";
+    private  $prenom = "";
+    private  $dateEmbauche;
+    private  $fonction="";
+    private  int $salaire;
+    private  $service;
+
+    public function __construct($nom,$prenom,$dateEmbauche,$fonction,$salaire,$service)
+    {
+        $this->$nom = $nom;
+        $this->$prenom = $prenom;
+        $this->$dateEmbauche = $dateEmbauche;
+        $this->$fonction = $fonction;
+        $this->$salaire = $salaire;
+        $this->$service = $service;
+
+    }
+
+    public function _anciennete()
+    {
+        $dateEmbauche = new DateTime($this->dateEmbauche);
+        $now = new DateTime();
+        $anciennete = ($now->diff($dateEmbauche))->format('%r%y years');
+        return $dateEmbauche;
+    }
+    
+}
+
+
+$damirdine = new Employe('ALI SOILIHI','Damirdine','15/03/2021','stagiaire',0,'formation');
+var_dump($damirdine->_anciennete());
+
+?>
